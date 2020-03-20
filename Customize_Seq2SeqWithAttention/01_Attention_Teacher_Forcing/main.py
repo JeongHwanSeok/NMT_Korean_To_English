2327d245cdb6a3065348424c0ec6c51e26b6529d
Jeong Hwan Seok <jhs0256@gmail.com>
2020-03-20 17:21:54 +0900

    Seq2Seq & Attention ¾÷·εå

---
+++ Customize_Seq2SeqWithAttention/01_Attention_Teacher_Forcing/main.py
@@ -0,0 +1,62 @@
+import argparse
+from Customize_Seq2SeqWithAttention.tools import Trainer, Translation
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # 1. File path
+    parser.add_argument('--data_path', default='../Data', type=str)
+    parser.add_argument('--dictionary_path', default='../Dictionary', type=str)
+    parser.add_argument('--src_train_filename', default='train_small.ko', type=str)
+    parser.add_argument('--tar_train_filename', default='train_small.en', type=str)
+    parser.add_argument('--src_val_filename', default='val_small.ko', type=str)
+    parser.add_argument('--tar_val_filename', default='val_small.en', type=str)
+    parser.add_argument('--model_path', default='Model', type=str)
+    parser.add_argument('--img_path', default='img', type=str)
+
+    # 2. Model Hyper Parameter
+    # 임베딩의 차원 rnn의 차원들을 전부 통일 시켜줘야함
+    parser.add_argument('--sequence_size', default=60, type=int)
+    parser.add_argument('--embedding_dim', default=500, type=int)
+
+    # 3. Eecoder
+    parser.add_argument('--encoder_rnn_dim', default=300, type=int)
+    parser.add_argument('--encoder_n_layers', default=3, type=int)
+
+    # 4. Decoder
+    parser.add_argument('--decoder_rnn_dim', default=300, type=int)
+    parser.add_argument('--decoder_n_layers', default=3, type=int)
+
+    # 5. Attention
+    parser.add_argument('--attention_score', default='general', type=str, choices=['dot', 'general'])
+    parser.add_argument('--get_attention', default=True, type=bool)
+
+    # 6. learning hyper parameter
+    parser.add_argument('--learning_method', default='Teacher_Forcing', type=str,
+                        choices=['Teacher_Forcing', 'Scheduled_Sampling'])
+    parser.add_argument('--learning_rate', default=0.001, type=float)
+    parser.add_argument('--epochs', default=30, type=int)
+    parser.add_argument('--batch_size', default=500, type=int)
+    parser.add_argument('--plot_count', default=6, type=int)
+    parser.add_argument('--train_step_print', default=10, type=int)
+    parser.add_argument('--val_step_print', default=100, type=int)
+    parser.add_argument('--step_save', default=1000, type=int)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    Trainer(args)
+
+    # translation = Translation(
+    #     checkpoint='Model/140000_model_1.pth',
+    #     dictionary_path='../Dictionary',
+    #     x_path='../Data/test.ko',
+    #     y_path='../Data/test.en',
+    #     beam_search=True,
+    #     k=3
+    # )
+    # # test = translation.transform('문장')
+    # # print(test)
+    # test = translation.batch_transform()
+
